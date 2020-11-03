a1222dfb9fbfc78ad2993d980f9ba427c4e5f353
Nick Morello <nickvmorello@gmail.com>
2020-11-02 20:43:50 -0700

    Changed parameters and constructors in Palindrome.java, switched from JUnit 5 to JUnit 4, added multiple tests to PalindromeTest.java. Added important documentation to each file.

--- Tests/com/company/PalindromeTest.java
+++ Tests/com/company/PalindromeTest.java
@@ -1,20 +1,73 @@
+/**
+ *  Author: Nicholas Morello
+ *  Class ID: ASU CSE360
+ *  PalindromeTest.java is a JavaDoc file used in testing the different parts of Palindrome.java file
+ */
+
 package com.company;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+public class PalindromeTest {
 
-class PalindromeTest {
+    private Palindrome pal;
+    private String input;
 
-    @Test
-    void isPalindrome() {
-        Palindrome pal = new Palindrome("Racecar");
-        assertEquals("Racecar", pal.getTestString());
+    @Before
+    public void setUpTest(){
+        input = null;
+        pal = new Palindrome();
     }
 
     @Test
-    void name() {
-        Palindrome pal = new Palindrome("Racecar");
-        
+    public void isAPalindrome(){
+        input = "racecar";
+        assertEquals(true,pal.isPalindrome(input));
+        System.out.println("Input \"" + input + "\" is a Palindrome." );
+    }
+
+    @Test
+    public void nonAlphaPalindrome(){
+        input = "&*$   @#?";
+        assertEquals(true,pal.isPalindrome(input));
+        System.out.println("Input \"" + input + "\" is a Palindrome." );
+    }
+
+    @Test
+    public void notPalindrome(){
+        input = "Puppy";
+        pal.isPalindrome(input);
+        assertEquals(false,pal.isPalindrome(input));
+        System.out.println("Input \"" + input + "\" is not a Palindrome." );
+    }
+
+    @Test
+    public void stringIsEmpty(){
+        input = "";
+        pal.isPalindrome(input);
+        assertEquals(true,pal.getTestString().isEmpty());
+        System.out.println("Input " + input + " is empty." );
+    }
+
+    @Test
+    public void isNull(){
+        assertNull(pal.getTestString());
+        System.out.println("Input value is Null" );
+    }
+
+    @Test
+    public void checkToLower(){
+        input = "AbCdEfGh";
+        pal.isPalindrome(input);
+        assertEquals("abcdefgh", pal.getTestString());
+        System.out.println("toLowerCase is functional");
+    }
+
+    @After
+    public void deleteOutputFile() {
+        input = null;
     }
 }
--- src/com/company/Palindrome.java
+++ src/com/company/Palindrome.java
@@ -1,35 +1,58 @@
+/**
+ *  Author: Nicholas Morello
+ *  Class ID: ASU CSE360
+ *  Palindrome.java is a class that checks if a string is a palindrome or not,
+ *      (a palindrome is a string that reads the same forwards and backwards ex: "racecar")
+ */
+
 package com.company;
 
 public class Palindrome {
     private String testString;
 
+    /**
+     * Constructor for the Palindrome class
+     * sets testString = to null to start
+     */
+    Palindrome(){
+        this.testString = null;
+    }
+
+    /**
+     * getTestString is a method that simply returns the value of the private String variable testString
+     * @return
+     */
     public String getTestString() {
         return testString;
     }
 
-    Palindrome(String testStr){
-        this.testString = testStr.toLowerCase();
-    }
-
-    public boolean isPalindrome(){
+    /**
+     * isPalindrome: testStr parameter is the String is being checked if it is a palindrome or not
+     * @return      returns true if the inputted string is a palindrome
+     *                  or if the string contains no alphanumeric characters
+     *              returns false if string is not a palindrome
+     */
+    public boolean isPalindrome(String testStr){
         int i = 0;
+        this.testString = testStr.toLowerCase();
         int len = this.testString.length();
         boolean isPal = true;
 
-        while ( (i<=len/2) && isPal){
-            if ((Character.compare(this.testString.charAt(i),this.testString.charAt(len-1-i) ) == 0)
-            || ((int)this.testString.charAt(i) < 48
-                    || ((int)this.testString.charAt(i) > 57 && ((int)this.testString.charAt(i) < 97))
-                    || (int)this.testString.charAt(i) > 122)){
-                System.out.println("True");
+        if (this.testString.isEmpty()){
+            isPal = true;
+        } else{
+            while ( (i<=len/2) && isPal){
+                if ((Character.compare(this.testString.charAt(i),this.testString.charAt(len-1-i) ) == 0)
+                        || ((int)this.testString.charAt(i) < 48
+                        || ((int)this.testString.charAt(i) > 57 && ((int)this.testString.charAt(i) < 97))
+                        || (int)this.testString.charAt(i) > 122)){
+                }
+                else {
+                    isPal = false;
+                }
+                i++ ;
             }
-            else {
-                System.out.println("False");
-                isPal = false;
-            }
-            i++;
         }
         return isPal;
     }
-
 }
